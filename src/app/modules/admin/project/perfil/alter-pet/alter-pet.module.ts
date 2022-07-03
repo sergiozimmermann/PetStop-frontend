091ea1a0891c8d47ba3e5b181931a66509f68a48
Sérgio Zimmermann <sergio-zimmermann22@hotmail.com>
2022-07-03 19:37:22 -0300

    commit teste

--- src/app/modules/admin/project/perfil/alter-pet/alter-pet.module.ts
+++ src/app/modules/admin/project/perfil/alter-pet/alter-pet.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AlterPetComponent } from './alter-pet.component';
-import { alterPetRoutes } from 'app/modules/admin/project/alter-pet/alter-pet.routing';
+import { alterPetRoutes } from 'app/modules/admin/project/perfil/alter-pet/alter-pet.routing';
 import { RouterModule } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
--- src/app/modules/admin/project/perfil/alter-pet/alter-pet.routing.ts
+++ src/app/modules/admin/project/perfil/alter-pet/alter-pet.routing.ts
@@ -1,7 +1,7 @@
 import { Routes, RouterModule, Route } from '@angular/router';
-import { AlterPetComponent } from 'app/modules/admin/project/alter-pet/alter-pet.component';
-import { ProjectResolver } from '../../dashboards/project/project.resolvers';
-import { MainComponent } from '../main/main.component';
+import { AlterPetComponent } from 'app/modules/admin/project/perfil/alter-pet/alter-pet.component';
+import { ProjectResolver } from '../../../dashboards/project/project.resolvers';
+import { MainComponent } from '../../main/main.component';
 const routes: Routes = [
 {
     path     : '',
